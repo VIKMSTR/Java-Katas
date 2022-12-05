3ae4c83c9b87cca62a2ce4691c33e5a6e0526c53
Viktor Krejčíř <krejcirviktor@gmail.com>
2022-12-05 13:52:13 +0100

    improved london city hacker

--- src/main/java/cz/vikmstr/kata/LondonCityHacker.java
+++ src/main/java/cz/vikmstr/kata/LondonCityHacker.java
@@ -1,21 +1,38 @@
 package cz.vikmstr.kata;
 
+import java.util.Arrays;
+
 public class LondonCityHacker {
+
+    /** Contains partial result of summing operation
+     * @param lastBus was last line bus or tube
+     * @param totalFare accumulated partial result of fare
+     */
+    record State (boolean lastBus, double totalFare){};
     public static String londonCityHacker(Object[] journey) {
         //code me up baby!
-        var lastBus = false;
-        var totalFare = 0.00;
-        for(Object o: journey){
-           if (o instanceof String) {
-               totalFare += 2.40;
-               lastBus = false;
-           }else if (o instanceof Integer){
-               if (!lastBus) {
-                    totalFare += 1.50;
-               }
-               lastBus = !lastBus;
-            }
+        var result = (State) Arrays.stream(journey)
+                .sequential()
+                .reduce(new State(false,0.00), LondonCityHacker::computeRide);
+        return String.format("£%2.2f", result.totalFare);
+        //coded baby!
+    }
+
+
+    /** Reduce method, that accumulates total fare for the whole ride.
+     * @param s State object, that accumulates fare and contains info about last ride (partial result)
+     * @param o current ride
+     * @return Updated state with information from current ride
+     */
+    static State computeRide(Object s, Object o){
+        var state = (State) s;
+        if (o instanceof String) { // tube station
+            return new State(false, state.totalFare+2.40);
         }
-        return String.format("£%2.2f", totalFare);
+        if (o instanceof Integer){ //bus station
+            var fare = state.lastBus ? state.totalFare : state.totalFare+1.50;
+            return new State(!state.lastBus,fare);
+        }
+        return state;
     }
 }
